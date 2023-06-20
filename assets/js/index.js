0ae742f615b8e05f3dd450ffce7aee8f34379a81
Jules <110929073+juse122@users.noreply.github.com>
2023-06-20 17:13:35 +0200

    Update sliders and contact area

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,49 @@
+// Swiper slider initialization //
+
 import { Swiper } from "./swiper-9.4.1-bundle.esm.browser.min.js";
 
-// https://codepen.io/i-did/pen/yLYRvVL
-// https://codepen.io/bradtraversy/pen/ExNVwgZ
-// https://stackoverflow.com/questions/72709006/video-touch-slider-with-html-css-javascript
-
-
-
-// Swiper slider #1 //
-
-const swiper = new Swiper(".swiper", {
+const swiperOne = new Swiper(".swiper-1", {
     direction: "horizontal",
     loop: true,
     pagination: {
-        el: ".swiper-pagination",
+        el: ".swiper-pagination-1",
         clickable: true,
     },
     navigation: {
-        prevEl: ".swiper-button-prev",
-        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev-1",
+        nextEl: ".swiper-button-next-1",
+    },
+});
+
+const swiperTwo = new Swiper(".swiper-2", {
+    direction: "horizontal",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination-2",
+        clickable: true,
+    },
+    navigation: {
+        prevEl: ".swiper-button-prev-2",
+        nextEl: ".swiper-button-next-2",
+    },
+});
+
+const swiperThree = new Swiper(".swiper-3", {
+    direction: "horizontal",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination-3",
+        clickable: true,
+    },
+    navigation: {
+        prevEl: ".swiper-button-prev-3",
+        nextEl: ".swiper-button-next-3",
     },
 });
 
 
 
-// Details elements plus/minus symbol switch on open/close //
+// Details elements toggle plus/minus symbol on open/close //
 
 const detailsElements = document.querySelectorAll("details");
 
@@ -45,5 +65,27 @@ detailsElements.forEach(element => {
 
 // Contact form submit message & error handling
 
-// <i class="fa-solid fa-circle-check"></i>
-// <i class="fa-solid fa-circle-exclamation"></i>
+const formElement = document.querySelector(".main-contact-form");
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const successElement = document.querySelector(".success");
+    const errorElement = document.querySelector(".error");
+
+    try {
+        fetch(formElement.action, {
+            method: formElement.method,
+            body: new FormData(formElement),
+        })
+
+        .then(() => {
+            errorElement.style.display = "none";
+            successElement.style.display = "flex";
+        });
+
+    } catch (error) {
+        successElement.style.display = "none";
+        errorElement.style.display = "flex";
+        errorElement.querySelector("p").innerText = `Fehler: ${ error }`;
+    };
+});
