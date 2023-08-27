dc6eeaea4683b3b92a38b05dbda7f6dba7660b28
Jules <110929073+juse122@users.noreply.github.com>
2023-08-28 00:10:20 +0200

    Try gitignore fix

--- assets/php/constants.php
+++
@@ -1,8 +0,0 @@
-// Google reCAPTCHA v3 public key //
-define("RECAPTCHA_KEY_PUBLIC", "6Ldi2rwmAAAAAEjjzoaCv8X3A5hQWIKKS4E5RKJC");
-
-// Google reCAPTCHA v3 secret key //
-define("RECAPTCHA_KEY_SECRET", "6Ldi2rwmAAAAABpdnmp38PViKIFdiOhvLwH4T2Qp");
-
-// Google reCAPTCHA v3 cut-off score //
-define("RECAPTCHA_SCORE", 0.6);
